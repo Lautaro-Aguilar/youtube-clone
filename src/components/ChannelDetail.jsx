e462542c4cd86a92be7f3bc1c06f9903e54166a6
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-22 00:53:38 -0300

    feat(Channel): :sparkles: Create channel details and channer card
    The channel details are created and the profile photo is displayed next to the channel details.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+import { Videos, ChannelCard } from "./";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Box } from "@mui/material";
+
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  console.log(channelDetail?.snippet?.thumbnails?.high.url);
+  return (
+    <Box minHeight='96vh'>
+      <Box>
+        <div
+          style={{
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${channelDetail?.snippet?.thumbnails?.high.url})`,
+            // filter: "blur(2px)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+        <Box display='flex' p='2'>
+          <Box sx={{ mr: { sm: "100px" } }} />
+          <Videos videos={videos} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
