bdd193d073da05683a632066fbe8396aa21730a6
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-21 21:20:49 -0300

    feat(Sidebar): :sparkles: list all categories and apply styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
 import { Stack } from "@mui/material";
 import React from "react";
+import { categories } from "../utils/constants";
+
+const selectedCategory = "New";
 
 const Sidebar = () => (
   <Stack
@@ -9,7 +12,32 @@ const Sidebar = () => (
       height: { sx: "auto", md: "95%" },
       flexDirection: { md: "column" },
     }}
-  ></Stack>
+  >
+    {categories.map((category) => (
+      <button
+        className='category-btn'
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
 );
 
 export default Sidebar;
